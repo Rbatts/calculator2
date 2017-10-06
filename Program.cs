b4a0078c7111b2a660f550ae491ca96c20c6e1e8
Rbatts <32482377+Rbatts@users.noreply.github.com>
2017-10-06 12:23:48 +0100

    finished task

--- Program.cs
+++ Program.cs
@@ -10,8 +10,23 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            string myString = "42";
-            int myInteger = int.Parse(myString);
+            //string myString = "42";
+            //int myInteger = int.Parse(myString);
+
+            Console.Write("Please enter a number\n");
+            string uResponse = Console.ReadLine();
+            int firstNumber = int.Parse(uResponse);
+
+            Console.Write("Please enter another number\n");
+            string uResponse2 = Console.ReadLine();
+            int secondNumber = int.Parse(uResponse2);
+
+            Console.WriteLine("Answer");
+            int answer = firstNumber* secondNumber;
+            Console.WriteLine(answer);
+            Console.ReadLine();
         }
+
+
     }
 }
