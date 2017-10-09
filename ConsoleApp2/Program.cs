24ce11872f87563d70b43a16bf2032cb38501239
Rbatts <32482377+Rbatts@users.noreply.github.com>
2017-10-09 09:18:10 +0100

    second calculator add done

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,51 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.Write("Welcome to the calculator\n");
+
+            Console.Write("Enter your opperator\n");
+            string opperator = Console.ReadLine();
+
+            Console.Write("How many numbers do you want to" + " " + opperator + " " + "?");
+            int count = int.Parse(Console.ReadLine());
+
+            int[] response = new int[count];
+            for (int index = 0; index < count; index++)
+            {
+
+                Console.Write("Please enter number " + (index + 1) + ": ");
+                response[index] = int.Parse(Console.ReadLine());
+
+            }
+
+            int answer = response[0];
+
+            for (int index = 1; index < count; index++)
+
+
+            if (opperator == "+")
+            {
+                answer = (answer + response[index]);
+            }
+            else if (opperator == "/")
+            {
+                answer = (answer / response[index]);
+            }
+            else if (opperator == "*")
+            {
+                answer = (answer * response[index]);
+            }
+            else if (opperator == "-")
+            {
+                answer = (answer - response[index]);
+            }
+
+            Console.WriteLine("Answer");
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        
         }
+
+
     }
 }
