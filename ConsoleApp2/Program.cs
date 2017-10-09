6da9f8eb7a06ddc3158ef06cbf783e7569413545
Rbatts <32482377+Rbatts@users.noreply.github.com>
2017-10-09 17:40:23 +0100

    Error message appears but not for all combinations of input

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -21,51 +21,55 @@ namespace ConsoleApp2
         {
             Console.Write("Welcome to the calculator\n");
 
-            Console.Write("Enter your opperator\n");
-            string opperator = Console.ReadLine();
-
-            Console.Write("How many numbers do you want to" + " " + opperator + " " + "?");
-            int count = int.Parse(Console.ReadLine());
-
-            int[] response = new int[count];
-
-            for (int index = 0; index < count; index++)
+            try
             {
-                Console.Write("Please enter number " + (index + 1) + ": ");
-                response[index] = int.Parse(Console.ReadLine());
-            }
+                Console.Write("Enter your opperator\n");
+                string opperator = Console.ReadLine();
+                
+                Console.Write("How many numbers do you want to" + " " + opperator + " " + "?");
+                int count = int.Parse(Console.ReadLine());
+                
 
-            int answer = response[0];
-            if (int.TryParse("", out answer))
-            {
-                Console.WriteLine("Well done");
-            }
-            else
-            {
-                Console.WriteLine("ohno");
-            }
-            for (int index = 1; index < count; index++)
+                int[] response = new int[count];
 
-                if (opperator == "+")
+                for (int index = 0; index < count; index++)
                 {
-                    answer = (answer + response[index]);
+                    Console.Write("Please enter number " + (index + 1) + ": ");
+                    response[index] = int.Parse(Console.ReadLine());
                 }
-                else if (opperator == "/")
-                {
-                    answer = (answer / response[index]);
-                }
-                else if (opperator == "*")
-                {
-                    answer = (answer * response[index]);
-                }
-                else if (opperator == "-")
-                {
-                    answer = (answer - response[index]);
-                }
-   
-            Console.WriteLine("Answer");
-            Console.WriteLine(answer);
-            Console.ReadLine();
+
+                int answer = response[0];
+
+                for (int index = 1; index < count; index++)
+
+                    if (opperator == "+")
+                    {
+                        answer = (answer + response[index]);
+                    }
+                    else if (opperator == "/")
+                    {
+                        answer = (answer / response[index]);
+                    }
+                    else if (opperator == "*")
+                    {
+                        answer = (answer * response[index]);
+                    }
+                    else if (opperator == "-")
+                    {
+                        answer = (answer - response[index]);
+                    }
+                    
+                Console.WriteLine("Answer");
+                Console.WriteLine(answer);
+                Console.ReadLine();
+
+            }
+            catch
+            {
+                Console.WriteLine("I'm sorry you've entered either your numbers or opperator incorrectly.\n Remember an opperator is either + - / * ");
+                Console.ReadLine();
+            }
+       
             Console.Clear();
 
             return true;
